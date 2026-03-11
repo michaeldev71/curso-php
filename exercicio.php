52514d154e70ac278a1b3797eb09ee0cfffe356e
Michael Jonathan <michaeljonathan.dev@gmail.com>
2026-03-11 11:26:06 -0300

    feat: inserido nova pagina em modulo 1 exercicio A

---
+++ exercicio.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="header">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercícios</h2>
+    </header>
+    
+    <main class="main">
+        <div class="conteudo">
+            
+        </div>
+    </main>
+    
+    <footer class="footer">
+        CODER & ALUNOS © <?= date("Y"); ?>
+    </footer>
+</body>
+</html>
