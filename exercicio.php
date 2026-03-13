9818a6408a4de6bd445aab2816b4dd2b801ebe68
Michael Jonathan <michaeljonathan.dev@gmail.com>
2026-03-13 10:21:30 -0300

    feat: inserido pasta teste + ajuste de CSS e html, assim como diretório em PHP para requisição

--- exercicio.php
+++ exercicio.php
@@ -7,17 +7,23 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
     <title>Curso PHP</title>
 </head>
-<body>
+<body class="exercicio">
     <header class="header">
         <h1>Curso PHP</h1>
         <h2>Visualização do Exercícios</h2>
     </header>
-    
+    <nav class="navegacao">
+        <a href="#" class="verde"> Sem formatação </a>
+        <a href="index.php" class="vermelho"> Voltar </a>
+    </nav>
     <main class="main">
         <div class="conteudo">
-            
+            <?php 
+                include(__DIR__ ."/{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
         </div>
     </main>
     
